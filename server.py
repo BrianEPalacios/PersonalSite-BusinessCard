386075846062c811c451f6e2ee5b798aab92e73d
Brian Palacios <b.estpalacios@gmail.com>
2023-02-14 19:38:43 -0600

    Add social media icons and develop back of card more

--- server.py
+++ server.py
@@ -7,5 +7,10 @@ def home_page():
     return render_template("index.html")
 
 
+@app.route('/portfolio')
+def portfolio_page():
+    return render_template(("portfolio.html"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
